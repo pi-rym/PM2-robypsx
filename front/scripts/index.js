3f36dd78c898068db2543977824c842c4dd1677d
robypsx <robypsx@gmail.com>
2024-03-21 03:07:19 -0600

    axios tarea

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,15 +1,29 @@
 // console.log(tempData);
 const renderCards = require("./renderCards");
-
+const axios = require("axios")
 // Peticion
-$.ajax({
-    url: "https://students-api.2.us-1.fl0.io/movies",
-    dataType: "json",
-    success: function (data) {
-      renderCards(data);
-    },
-    error: function (error) {
+
+// $.ajax({
+//     url: "https://students-api.2.us-1.fl0.io/movies",
+//     dataType: "json",
+//     success: function (data) {
+//       renderCards(data);
+//     },
+//     error: function (error) {
+//       console.error("Error fetching movies:", error);
+//       $("#error-message").removeClass("d-none");
+//     }
+//   });
+
+const fetchData = () => {
+  axios.get("https://students-api.2.us-1.fl0.io/movies")
+    .then((response) => {
+      renderCards(response.data);
+    })
+    .catch((error) => {
       console.error("Error fetching movies:", error);
       $("#error-message").removeClass("d-none");
-    }
-  });
+    });
+};
+
+fetchData();
